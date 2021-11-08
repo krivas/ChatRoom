4335f7867a650ea0d810c5a6f84b3758a22afefd
KR319198 <kevin_rivas@bhdleon.com.do>
2021-11-08 18:24:05 -0400

    Add identity,domain and data projects

---
+++ ChatRoom.Data/ChatRoomContext.cs
@@ -0,0 +1,17 @@
+﻿using ChatRoom.Domain;
+using ChatRoom.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ChatRoom.Data
+{
+    public class ChatRoomContext:DbContext
+    {
+        public DbSet<ApplicationUser> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SERSQLDEV02\\DEV12INS01;Database=Sentinel_Intermedia;User Id=SentinelRiesgo;Password=SentinelRiesgo;");
+        }
+    }
+}
---
+++ ChatRoom.Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace ChatRoom.Identity
+{
+    public class ApplicationUser :IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
